d71b965b381c84eab5fc191441f2bc21604654c6
Noxmortis <iramortis@gmail.com>
2013-06-22 14:19:29 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gofast2::Application.config.secret_token = '3d4c0714202eff352db0fe8438f463d6eee3b74c7955939531b5b58c8d3116265e09fc8a158fb8c57063b5f8bd44f43a2a5ecd5a4f24aaf164d26786a92ae7a1'
