361e8ab678df413484a44af39e50ae5caa2e322e
Noxmortis <iramortis@gmail.com>
2013-06-23 10:41:20 -0400

    added quotes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,45 @@
 Gofast2::Application.routes.draw do
+  get "quotes/index"
+
+  get "index/index"
+
+  get "index/about"
+
+  get "index/contact"
+
+  resources :drivers
+
+
+  resources :trailers
+
+
+  resources :trucks
+
+
+  resources :job_postings
+
+
+  resources :maintenances
+
+
+  resources :trips
+
+
+  resources :shipments
+
+
+  resources :skids
+
+
+  resources :announcements
+
+
+  resources :cartages
+
+
+  resources :companies
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,11 +89,11 @@ Gofast2::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'index#index'
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+  match ':controller(/:action(/:id))(.:format)'
 end
