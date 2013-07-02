8f1e758a26bd68c1bcce78f4398c7270b0e2cdd5
Noxmortis <iramortis@gmail.com>
2013-07-02 18:32:49 -0400

    change announcement html

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Announcement.create(date_posted: '20130702', content: 'This is the body of the announcement', title: 'Intro Post', posted_by: '1')
