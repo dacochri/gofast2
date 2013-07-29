ce6e01fcc1ca8b35a37084878152b23b286cd144
Christian DaCosta <dacochri@gmail.com>
2013-07-29 09:28:35 -0400

    remove driver pay

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,3 +1,3 @@
 class Trip < ActiveRecord::Base
-  attr_accessible :driver_pay, :end_date, :expense_id, :kilometres_accumulated, :load_bar_count, :start_date, :trailer_id, :truck_id
+  attr_accessible :end_date, :expense_id, :kilometres_accumulated, :load_bar_count, :start_date, :trailer_id, :truck_id
 end
