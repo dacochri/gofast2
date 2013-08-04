69a1c7a204268708e525e3be9ba8c0fc35a9d22d
Noxmortis <iramortis@gmail.com>
2013-08-04 10:56:37 -0400

    #1 - views

--- app/models/skid.rb
+++ app/models/skid.rb
@@ -1,3 +1,3 @@
 class Skid < ActiveRecord::Base
-  attr_accessible :company_id, :delivery_location, :delivery_time, :oversized, :pickup_location, :pickup_time, :product_desc, :shipment_id, :skid_count, :warehouse_delivery_time, :warehouse_pickup_time, :weight
+  attr_accessible :delivery_location, :delivery_time, :oversized, :pickup_location, :pickup_time, :product_desc, :shipment_id, :skid_count, :warehouse_delivery_time, :warehouse_pickup_time, :weight
 end
