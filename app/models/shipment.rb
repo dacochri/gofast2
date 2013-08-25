8c49a8dd5c8ef7e10fc9586fd8271a332c53a608
Noxmortis <iramortis@gmail.com>
2013-08-25 14:32:41 -0400

    remove shipment_no

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,3 +1,3 @@
 class Shipment < ActiveRecord::Base
-  attr_accessible :broker_id, :customer_id, :distance, :partial, :rate, :shipment_no, :trip_id
+  attr_accessible :broker_id, :customer_id, :distance, :partial, :rate, :trip_id
 end
