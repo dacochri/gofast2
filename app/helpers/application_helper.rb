5354decb2b08ae56a0c04395d53cb1258a9d818e
Bojan <vecanskib@gmail.com>
2013-09-17 17:25:20 -0400

    Comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,22 @@
 module ApplicationHelper
+  #This method will print out the headers in the table on the pages, it will make the sortable as well
 	def sortable(column, title = nil, table, col)
+    #Takes the name of the column and transforms it into a readable word i.e. my_column become My Column
 	  title ||= column.titleize
+    #Stores the value of the column by which it is sorting
 	  data_sort = column == sort_column(table, col) ? sort_direction : nil
+    #Stores the value of the direction of by which the column is being sorted by
     direction = column == sort_column(table, col) && sort_direction == 'asc' ? 'desc' : 'asc'
+    #Prints out the column header as a click-able link which is used to sort the table
 	  link_to title, params.merge(:sort => column, :direction => direction), :'data-sort' => data_sort
 	end
 
+  #Selects by which column to sort the page, if nothing is selected than it uses the default provided in the method argument
 	def sort_column(table, col)
 		table.column_names.include?(params[:sort]) ? params[:sort] : col
 	end
 
+  #Selects the sort direction for the filters, if nothing is assigned then it chooses ascending as default
 	def sort_direction
 		%w[asc desc].include?(params[:direction]) ? params[:direction] : 'asc'
 	end
--- lib/searchable.rb
+++ lib/searchable.rb
@@ -2,22 +2,28 @@ module Searchable
   extend ActiveSupport::Concern
 
   module ClassMethods
+    #This method is used to search for the value inside the database
     def search(keyword, column_name)
+      #checks if the column selected exists in the database
       if column_names.include?(column_name.to_s)
+        #Performs a Search Query on the table based on the user input
         where("#{table_name}.#{column_name} LIKE ?", "%#{keyword}%")
       else
+        #if the column did not exist, it returns all records
         scoped
       end
     end
-
+    #Loops through string array provided and removes those columns from the list.
     def searchable_columns(unwanted_columns)
       column_names.reject{ |column| unwanted_columns.include?(column) }
     end
-
+    #Returns a map of columns that are able to be searched, this shows in the drop down on each page
     def translated_searchable_columns(unwanted_columns)
 			unwanted_columns.push('created_at')
 			unwanted_columns.push('updated_at')
+      #Calls the method that removes the unwanted columns
       columns = searchable_columns(unwanted_columns)
+      #Maps the columns and returns it as a hash
       columns.map{ |column| [human_attribute_name(column.to_sym), column] }
     end
   end
