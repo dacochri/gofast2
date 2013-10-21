e26f672ef6b8408d09d6022db5cce1098116e15d
Noxmortis <iramortis@gmail.com>
2013-10-21 13:04:36 -0400

    Announcements on Home Page

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,10 +1,13 @@
 class IndexController < ApplicationController
   def index
+    @announcements = Announcement.limit(6).order('id DESC').all
   end
 
   def about
+    
   end
 
   def contact
+    
   end
 end
