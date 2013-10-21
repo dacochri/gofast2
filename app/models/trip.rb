50f8b4bda1373a2ac87790cd065273e5ede55c8f
Noxmortis <iramortis@gmail.com>
2013-10-21 14:23:04 -0400

    trip comment not mandatory

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -10,5 +10,5 @@ class Trip < ActiveRecord::Base
   validates :fuel_cost, :format => { :with => ValidationValues.double, :message => message }
   validates :misc_cost, :format => { :with => ValidationValues.double, :message => message }, :allow_blank => true
   validates :start_date, :end_date, :format => { :with => ValidationValues.date, :message => message }
-  validates :comments, :format => { :with => ValidationValues.content, :message => message }
+  validates :comments, :format => { :with => ValidationValues.content, :message => message }, :allow_blank => true
 end
