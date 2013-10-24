e5315577280283e8f51bd5d52c2cf3dd174956cc
Noxmortis <iramortis@gmail.com>
2013-10-24 14:22:55 -0400

    remove first/last name

--- app/models/cartage.rb
+++ app/models/cartage.rb
@@ -8,6 +8,6 @@ class Cartage < ActiveRecord::Base
   validates :company_id, :cost, :delivery_time, :driver_name, :pickup_time, :trailer_id, :presence => true
   validates :company_id, :trailer_id, :numericality => true
   validates :cost, :format => { :with => ValidationValues.double, :message => message }
-  validates :driver_name, :format => { :with => ValidationValues.last_name, :message => message }
+  validates :driver_name, :format => { :with => ValidationValues.name, :message => message }
   validates :delivery_time, :pickup_time, :format => { :with => ValidationValues.date_time, :message => message }
 end
