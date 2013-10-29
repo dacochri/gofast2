005cca5899091147ab471deca058be535aa2209e
Noxmortis <iramortis@gmail.com>
2013-10-29 10:29:51 -0400

    #8 edit user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,8 +30,26 @@ class UsersController < ApplicationController
     end
   end
 
+
   # GET /users/1/edit
   def edit
+    @user = User.find(params[:id])
+  end
+
+  # PUT /users/1
+  # PUT /users/1.json
+  def update
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html { redirect_to '/user', notice: 'User was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   # DELETE /users/1
