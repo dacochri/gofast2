bc47f0a6ec010eda3266ef6a7a9a709fb9beaeb4
Noxmortis <iramortis@gmail.com>
2013-11-03 16:12:38 -0500

    #52

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
+ 
+  helper_method :format_time
+
   # Since the date format the calendar input to MM-DD-YYYY, we want to change it to YYYY-MM-DD
   def format_date(date)
     # Only attempt to change the format if it matches a date format beforehand
@@ -11,6 +13,10 @@ class ApplicationController < ActionController::Base
     
     # Either return the value that was passed in (if a date wasn't passed in), or the newly changed format
     date
+  end
+
+  def format_time(date_time)
+    date_time.strftime('%Y-%m-%d %H:%M %Z')
   end
 
   def redirect_not_admin
